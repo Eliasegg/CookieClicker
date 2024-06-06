4425c4e96afbeb22ef24c5fa2ed3d27fa2ef2a24
Eliasegg <elias-eeg@hotmail.com>
2024-06-05 19:03:00 -0600

    Inicializar proyecto

---
+++ src/cookieclicker/CookieClicker.java
@@ -0,0 +1,16 @@
+package cookieclicker;
+
+/**
+ *
+ * @author elias
+ */
+public class CookieClicker {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
